223de870615a01209c56f06f8910cd8f2c6cf8eb
kurea66 <passionately.66.meltdown.4949@gmail.com>
2020-11-29 16:27:03 +0900

    finish_Q01

--- Q01/Main.java
+++ Q01/Main.java
@@ -1,8 +1,8 @@
 import java.util.*;
 public class Main{
 	public static void main(String[] args){
-		//要素数5のint配列作成(配列変数名はarr)
-		//index2に10を代入
+		int [] arr=new int [5];//要素数5のint配列作成(配列変数名はarr)
+		arr[2]=10;//index2に10を代入
 		//出力
 		System.out.println(Arrays.toString(arr));
 	}
