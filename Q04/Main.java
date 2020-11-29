2787aa74aed7a7effc3a324ad9c8bbf996a26df7
kurea66 <passionately.66.meltdown.4949@gmail.com>
2020-11-29 16:41:26 +0900

    finish_Q04

--- Q04/Main.java
+++ Q04/Main.java
@@ -4,9 +4,11 @@ public class Main{
 		System.out.print("何日間>");
 		int days=new Scanner(System.in).nextInt();
 		//double型の配列を作成
+		double[] weights=new double[days];
 		for(int i=0;i<weights.length;i++){
 			System.out.print(i+1+"日目の体重を入力>");
 			//Scannerを使ってdoubleの値を取得
+			weights[i]=new Scanner(System.in).nextDouble();
 			//その値を配列に格納する
 		}
 		System.out.println(Arrays.toString(weights));
