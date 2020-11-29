889f421aed15a039f593772dec02070dc3894408
kurea66 <passionately.66.meltdown.4949@gmail.com>
2020-11-29 16:55:40 +0900

    finish_Q07

--- Q07/Main.java
+++ Q07/Main.java
@@ -3,6 +3,7 @@ public class Main{
 	public static void main(String[] args){
 		//要素数3のboolean配列を作成(配列変数名はbools)
 		//配列の最後の要素にtrueを代入
+		boolean[] bools={false,false,true};
 		System.out.println(Arrays.toString(bools));
 	}
 }
